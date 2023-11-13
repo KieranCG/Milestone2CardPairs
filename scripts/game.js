7d03f824c1397ed547adc450f7e48fc2d4834782
KieranCG <>
2023-11-13 11:19:33 +0000

    Added an async load command with event listeners to allow the overlay to function

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,15 @@
+// To allow the overlay to work the javascript needs to load at the same time.
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
+}
+
+function ready() {
+
+}
+
+// Intial test for Jest Environment
 function sum(a, b) {
     return a + b;
 }
